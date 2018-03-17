543539cdfbbd30d91f68356617bfacc520ca887c
Unknown <arpanladani@gmail.com>
2018-03-17 22:48:26 +0530

    Initial commit

---
+++ Assets/IAPDemo/Scripts/WeaponShop/MakeWeaponObject.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MakeWeaponObject {
+	#if UNITY_EDITOR
+	[MenuItem("Assets/Create/Weapon Object")]
+	public static void CreateWeaponObject()
+	{
+		WeaponObject asset = ScriptableObject.CreateInstance<WeaponObject> ();
+
+		AssetDatabase.CreateAsset (asset, "Assets/NewWeaponObject.asset");
+		AssetDatabase.SaveAssets ();
+		EditorUtility.FocusProjectWindow ();
+		Selection.activeObject = asset;
+
+	}
+	#endif
+}
